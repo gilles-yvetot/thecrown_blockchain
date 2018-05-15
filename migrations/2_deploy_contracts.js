18b4b5dcca4a6d616a97332462f3ed48f37be5f5
Gilles Yvetot <g.yvetot@gmail.com>
2018-05-15 13:20:57 -0700

    [ADD] assign contract ownership to ICO

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,32 @@
 const CrownTokenCrowdsale = artifacts.require('./CrownTokenCrowdsale.sol');
 const CrownToken = artifacts.require('./CrownToken.sol');
 
-module.exports = function(deployer, network, accounts) {
-    const rate = new web3.BigNumber(1000);
-    const wallet = accounts[1];
+module.exports = function (deployer, network, accounts) {
+  const rate = new web3.BigNumber(1000);
+  const wallet = accounts[1];
 
-    return deployer
-        .then(() => {
-            return deployer.deploy(CrownToken);
+  return deployer
+    .then(() => {
+      return deployer.deploy(CrownToken);
+    })
+    .then(() => {
+      return deployer.deploy(
+        CrownTokenCrowdsale,
+        rate,
+        wallet,
+        CrownToken.address
+      );
+    })
+    .then(() => {
+      return CrownTokenCrowdsale.deployed()
+    })
+    .then(crowdsale => {
+      return crowdsale.token()
+        .then(tokenAddress => {
+          console.log('tokenAddress', tokenAddress);
+          console.log('crowdsale.address', crowdsale.address);
+          const crownTokenInstance = CrownToken.at(tokenAddress)    
+          return crownTokenInstance.transferOwnership(crowdsale.address)
         })
-        .then(() => {
-            return deployer.deploy(
-                CrownTokenCrowdsale,
-                rate,
-                wallet,
-                CrownToken.address
-            );
-        });
+    })
 };
